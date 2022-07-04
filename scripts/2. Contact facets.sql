c9631acc04e5e4e6cd291bb433a3261f30de0c19
Anna Gevel <anngevel@gmail.com>
2022-07-04 11:08:33 +0100

    Update 2. Contact facets.sql

--- scripts/2. Contact facets.sql
+++ scripts/2. Contact facets.sql
@@ -1,1 +1,7 @@
+USE [database_name] -- replace with your Shard DB name, you may need to check all your Shard DBs
+GO
 
+SELECT *
+FROM [xdb_collection].[ContactFacets]
+WHERE ContactId = '0E3CC254-C440-0000-0000-062C9FE15922' -- replace with your contact ID
+AND FacetKey = 'TestCombinations'
