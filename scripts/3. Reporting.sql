85e45d593678a3f42f5a59bc97a431905182e658
Anna Gevel <anngevel@gmail.com>
2022-07-06 13:25:43 +0100

    Update 3. Reporting.sql
    added Fact_TestConversions table script

--- scripts/3. Reporting.sql
+++ scripts/3. Reporting.sql
@@ -6,3 +6,11 @@ GO
 SELECT *
 FROM [dbo].[Fact_MvTesting]
 WHERE TestSetId = 'DB071F98-C2CE-4DBB-8873-2E378F88CEB4' -- replace with your test ID
+
+
+-- [Fact_TestConversions] stores aggregated number of triggered goals, associated visits and engagrement value
+-- for each test variant and goal ID
+SELECT *
+FROM [dbo].[Fact_TestConversions]
+WHERE TestSetId = 'DB071F98-C2CE-4DBB-8873-2E378F88CEB4' -- replace with your test ID
+AND GoalId = '3430F877-4767-4B2E-A121-FDA73122FEFB' -- optionally you can filter by a specific goal ID
