8df057d0a05350e841c2b66ac79ea1fc85479a4d
Anna Gevel <anngevel@gmail.com>
2022-07-06 13:00:08 +0100

    Update 3. Reporting.sql
    Fact_MvTesting table script

--- scripts/3. Reporting.sql
+++ scripts/3. Reporting.sql
@@ -1,1 +1,8 @@
+USE [database_name] -- replace with your Reporting DB name
+GO
 
+-- [Fact_MvTesting] contains aggregated number of visits, engagement value, bounces, etc. for each test variant
+-- TestSetId is ID of test definition item and TestValues represent each of the test variants
+SELECT *
+  FROM [dbo].[Fact_MvTesting]
+  WHERE TestSetId = 'DB071F98-C2CE-4DBB-8873-2E378F88CEB4' -- replace with your test ID
